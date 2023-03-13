194168aa3e9e834d0a31beb7eb509248fd5cba10
Jeffery Allbright <jefferyallbright96@gmail.com>
2023-03-13 16:31:29 -0400

    initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+    public class Calculator {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            double num1, num2, result;
+            char operator;
+
+            System.out.print("Enter first number: ");
+            num1 = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            num2 = scanner.nextDouble();
+
+            System.out.print("Enter an operator (+, -, *, /): ");
+            operator = scanner.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    System.out.println(num1 + " + " + num2 + " = " + result);
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    System.out.println(num1 + " - " + num2 + " = " + result);
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    System.out.println(num1 + " * " + num2 + " = " + result);
+                    break;
+
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("Error: division by zero");
+                    } else {
+                        result = num1 / num2;
+                        System.out.println(num1 + " / " + num2 + " = " + result);
+                    }
+                    break;
+
+                default:
+                    System.out.println("Invalid operator");
+                    break;
+            }
+
+            scanner.close();
+        }
+    }
+
